8ae1dae8591ff021fe4ca31b3fb6566312845ebf
Jeremy Banka <hello@jeremybanka.com>
2024-10-14 16:19:53 -0700

    ✨ add flush

--- packages/agency/__tests__/grunt.test.ts
+++ packages/agency/__tests__/grunt.test.ts
@@ -37,6 +37,7 @@ describe(`ai`, () => {
 		})
 		console.log(evaluation)
 		expect(evaluation.passed).toBe(true)
+		istanbulEnthusiast.flushTestFiles()
 	}, 20_000)
 	test(`ai testing conversation`, async () => {
 		const jobSeeker = new Grunt(
@@ -99,5 +100,6 @@ describe(`ai`, () => {
 		})
 		console.log(evaluation)
 		expect(evaluation.passed).toBe(true)
+		careerAdvisor.flushTestFiles()
 	}, 120_000)
 })
