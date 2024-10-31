1463bad3e71cf81b048cd9178ceb1e5b0c00a8e0
Joshua Bertsche <jjjbertsche@gmail.com>
2024-10-31 18:28:48 +0000

    Update openai pricing facts (#365)

--- packages/safegen/src/openai/pricing-facts.ts
+++ packages/safegen/src/openai/pricing-facts.ts
@@ -1,4 +1,4 @@
-// 13 October 2023
+// 30 October 2023
 // https://openai.com/pricing
 
 import type { ChatModel } from "openai/resources/index"
@@ -9,10 +9,35 @@ export const OPEN_AI_PRICING_FACTS = {
 		promptPricePerTokenCached: 0.00125 / 1000,
 		completionPricePerToken: 0.01 / 1000,
 	},
+	"gpt-4o-2024-08-06": {
+		promptPricePerToken: 0.0025 / 1000,
+		promptPricePerTokenCached: 0.00125 / 1000,
+		completionPricePerToken: 0.01 / 1000,
+	},
+	"gpt-4o-2024-05-13": {
+		promptPricePerToken: 0.005 / 1000,
+		promptPricePerTokenCached: 0.005 / 1000,
+		completionPricePerToken: 0.015 / 1000,
+	},
 	"gpt-4o-mini": {
 		promptPricePerToken: 0.00015 / 1000,
 		promptPricePerTokenCached: 0.000075 / 1000,
 		completionPricePerToken: 0.0006 / 1000,
+	},
+	"gpt-4o-mini-2024-07-18": {
+		promptPricePerToken: 0.00015 / 1000,
+		promptPricePerTokenCached: 0.000075 / 1000,
+		completionPricePerToken: 0.0006 / 1000,
+	},
+	"o1-preview": {
+		promptPricePerToken: 0.015 / 1000,
+		promptPricePerTokenCached: 0.0075 / 1000,
+		completionPricePerToken: 0.06 / 1000,
+	},
+	"o1-preview-2024-09-12": {
+		promptPricePerToken: 0.015 / 1000,
+		promptPricePerTokenCached: 0.0075 / 1000,
+		completionPricePerToken: 0.06 / 1000,
 	},
 } satisfies Partial<
 	Record<
