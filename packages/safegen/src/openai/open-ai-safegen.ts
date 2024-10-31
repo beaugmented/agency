a6d26743b49c2220ed3e01005980e5251b286148
Joshua Bertsche <jjjbertsche@gmail.com>
2024-10-31 19:25:12 +0000

    Only allow openai models we have pricing data for (#367)
    * Only allow openai models we have pricing data for
    
    * ✅ add semver test
    
    * 🦋
    
    ---------
    
    Co-authored-by: Jeremy Banka <hello@jeremybanka.com>

--- packages/safegen/src/openai/open-ai-safegen.ts
+++ packages/safegen/src/openai/open-ai-safegen.ts
@@ -1,11 +1,13 @@
-import type { ChatModel } from "openai/resources/index"
-
 import { createSafeDataGenerator } from "../safegen"
 import { buildOpenAiRequestParams } from "./build-open-ai-request-params"
+import type { OPEN_AI_PRICING_FACTS } from "./pricing-facts"
 import { setUpOpenAiJsonGenerator } from "./set-up-open-ai-generator"
 
 let getUnknownJsonFromOpenAi: ReturnType<typeof setUpOpenAiJsonGenerator>
-export const openaiSafeGen = (model: ChatModel, apiKey: string) =>
+export const openaiSafeGen = (
+	model: keyof typeof OPEN_AI_PRICING_FACTS,
+	apiKey: string,
+) =>
 	createSafeDataGenerator((...params) => {
 		if (!getUnknownJsonFromOpenAi) {
 			getUnknownJsonFromOpenAi = setUpOpenAiJsonGenerator(apiKey)
