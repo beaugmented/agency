a76ec357ef9e02eac91020d3ad340768963e70ec
Jeremy Banka <hello@jeremybanka.com>
2025-06-19 01:24:48 -0700

    ✨ add primitive generation into google

--- packages/safegen/src/google/google-safe-generator.ts
+++ packages/safegen/src/google/google-safe-generator.ts
@@ -1,10 +1,14 @@
-import type { GenerateContentResponseUsageMetadata } from "@google/genai"
+import type {
+	GenerateContentParameters,
+	GenerateContentResponseUsageMetadata,
+} from "@google/genai"
 import { GoogleGenAI } from "@google/genai"
 import type { StandardSchemaV1 } from "@standard-schema/spec"
 import type { CacheMode, Squirreled } from "varmint"
 import { Squirrel } from "varmint"
 import type { ZodType } from "zod/v4"
 
+import { booleanGen, chooseGen, numberGen } from "../primitives"
 import type {
 	GenerateFromSchema,
 	SafeGenerator,
@@ -12,7 +16,7 @@ import type {
 } from "../safegen"
 import { createSafeDataGenerator } from "../safegen"
 import { buildGoogleRequestParams } from "./build-google-request-params"
-import type { ModelName } from "./google-pricing-facts"
+import { calculateInferencePrice, type ModelName } from "./google-pricing-facts"
 import type { GetUnknownJsonFromGoogle } from "./set-up-google-json-generator"
 import { setUpGoogleJsonGenerator } from "./set-up-google-json-generator"
 
@@ -29,14 +33,19 @@ export type GoogleSafeGenOptions<S extends StandardSchemaV1 = ZodType> = {
 	toJsonSchema?: ToJsonSchema<S>
 }
 
+export type GetCompletion = typeof GoogleGenAI.prototype.models.generateContent
+
 export class GoogleSafeGenerator implements SafeGenerator {
 	public usdBudget: number
 	public usdMinimum: number
+	public getCompletionSquirreled: Squirreled<GetCompletion>
 	public getUnknownJsonFromGoogle: GetUnknownJsonFromGoogle
 	public getUnknownJsonFromGoogleSquirreled: Squirreled<GetUnknownJsonFromGoogle>
 	public squirrel: Squirrel
+	public model: ModelName
 	public client?: GoogleGenAI
 	public lastUsage?: GenerateContentResponseUsageMetadata | undefined
+	public logger: Pick<Console, `error` | `info` | `warn`>
 
 	public constructor({
 		model,
@@ -51,13 +60,15 @@ export class GoogleSafeGenerator implements SafeGenerator {
 		this.usdMinimum = usdMinimum
 		this.squirrel = new Squirrel(cachingMode)
 		let client = clientCache.get(apiKey)
-		if (cachingMode !== `read`) {
-			if (!client) {
-				client = new GoogleGenAI({ apiKey })
-				clientCache.set(apiKey, client)
-			}
-			this.client = client
+		if (!client) {
+			client = new GoogleGenAI({ apiKey })
+			clientCache.set(apiKey, client)
 		}
+		this.client = client
+		this.getCompletionSquirreled = this.squirrel.add(
+			`google-safegen`,
+			this.client.models.generateContent.bind(this.client.models),
+		)
 		this.getUnknownJsonFromGoogle = setUpGoogleJsonGenerator(this.client)
 		this.getUnknownJsonFromGoogleSquirreled = this.squirrel.add(
 			cacheKey,
@@ -82,7 +93,97 @@ export class GoogleSafeGenerator implements SafeGenerator {
 			this.usdBudget -= usdPrice
 			return data
 		}, logger)
+		this.object = this.from
 	}
 
+	public getCompletion = async (
+		key: string,
+		body: GenerateContentParameters,
+	): Promise<string> => {
+		if (this.usdBudget < this.usdMinimum) {
+			this.logger.warn(`SafeGen budget exhausted`)
+			return ``
+		}
+		const completion = await this.getCompletionSquirreled.for(key).get(body)
+		const { text, usageMetadata: usage } = completion
+		if (usage) {
+			const usdPrice = calculateInferencePrice(usage, body.model)
+			this.usdBudget -= usdPrice
+		}
+		if (!text) {
+			throw new Error(`No text message found in completion`)
+		}
+		return text
+	}
+
+	/** @deprecated Use `SafeGenerator.object()` instead */
 	public from: GenerateFromSchema
+	public object: GenerateFromSchema
+
+	public boolean(instruction: string): Promise<Error | boolean> {
+		return booleanGen(instruction, async (prompt, filename) => {
+			const completion = await this.getCompletion(filename, {
+				model: this.model,
+				contents: {
+					role: `user`,
+					parts: [{ text: prompt }],
+				},
+				config: { maxOutputTokens: 1 },
+			})
+			return completion.toLowerCase()
+		})
+	}
+
+	public number(
+		instruction: string,
+		min: number,
+		max: number,
+	): Promise<Error | number> {
+		return numberGen(instruction, min, max, async (prompt, filename) =>
+			this.getCompletion(filename, {
+				model: this.model,
+				contents: {
+					role: `user`,
+					parts: [{ text: prompt }],
+				},
+				config: { maxOutputTokens: 6 },
+			}),
+		)
+	}
+
+	public choose<T extends (number | string)[]>(
+		instruction: string,
+		options: T,
+		min?: 1,
+		max?: 1,
+	): Promise<Error | T[number]>
+	public choose<T extends (number | string)[]>(
+		instruction: string,
+		options: T,
+		min: number,
+		max?: number,
+	): Promise<Error | T[number][]>
+	public choose<T extends (number | string)[]>(
+		instruction: string,
+		options: T,
+		min = 1,
+		max = min,
+	): Promise<Error | T[number] | T[number][]> {
+		return chooseGen(
+			instruction,
+			options,
+			min,
+			max,
+			async (prompt, filename, maxTokens) =>
+				this.getCompletion(filename, {
+					model: this.model,
+					contents: {
+						role: `user`,
+						parts: [{ text: prompt }],
+					},
+					config: { maxOutputTokens: maxTokens },
+				}),
+			this.logger,
+		)
+	}
 }
